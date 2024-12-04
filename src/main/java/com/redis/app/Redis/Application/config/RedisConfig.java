cb54234b38fdc9209740df012efe91fdf5fac7b3
princekhj555 <princekhj555@gmail.com>
2024-12-04 19:31:45 +0530

    fixed config files

--- src/main/java/com/redis/app/Redis/Application/config/RedisConfig.java
+++ src/main/java/com/redis/app/Redis/Application/config/RedisConfig.java
@@ -1,19 +1,34 @@
 package com.redis.app.Redis.Application.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
 
     @Bean
     public RedisConnectionFactory connectionFactory(){
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+
+        log.info("Redis Host :: {} and Port : {} ",redisHost,redisPort);
+        return new LettuceConnectionFactory(config);
     }
     @Bean
     public RedisTemplate<String,Object> redisTemplate(){
